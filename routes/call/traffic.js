89df6bbae6abc4afcd52e7a20531ca706dc93f47
Jonghun Yoon <ongnya@gmail.com>
2018-08-10 16:34:06 +0900

    날씨,교통량데이터변환

--- routes/call/traffic.js
+++ routes/call/traffic.js
@@ -6,8 +6,37 @@ const CONFIG = require("../../common/config");
 
 // 1. trim
 // 2. parseInt(), parseFloat()
-var setArray = function() {
+var setArray = function(body) {
+	console.log("traffic setArray start");
+	var rowData = body.VolInfo.row ;
+	var rowDataArray = new Array();
+	for (var i = 0 ; i < rowData.length ; i++) {
+		var rowData2 = new Object();
+		
+		rowData2.spot_num = rowData[i].spot_num;
+		rowData2.ymd = rowData[i].ymd;
+		if(rowData[i].hh =="") {
+			rowData2.hh = parseInt("0");
+		} else {
+			rowData2.hh = parseInt(rowData[i].hh);
+		}
+		rowData2.io_type = rowData[i].io_type;
+		if(rowData[i].lane_num =="") {
+			rowData2.lane_num = parseInt("0");
+		} else {
+			rowData2.lane_num = parseInt(rowData[i].lane_num);
+		}
+		if(rowData[i].vol =="") {
+			rowData2.vol = parseInt("0");
+		} else {
+			rowData2.vol = parseInt(rowData[i].vol);
+		}
+		rowDataArray.push(rowData2);
+	}
 	
+	var rowData3 = JSON.stringify(rowDataArray);
+	console.log("traffic setArray end");
+	return rowData3;
 }
 
 router.get("/", function(req, res, next) {
@@ -22,6 +51,8 @@ router.get("/", function(req, res, next) {
 			console.log(getErr);
 			res.status(500).send();
 		} else {
+//			setArray(getRs);
+			console.log(setArray(getRs));
 			cFunc.writeXmlToJson("traffic", "traffic_"+date+"_"+time, getRs, function(writeErr, writeRs) {
 				if( writeErr ) {
 					console.log(writeErr);
--- routes/call/weather.js
+++ routes/call/weather.js
@@ -9,8 +9,53 @@ const CONFIG = require("../../common/config");
 var setArray = function(body) {
 	console.log("weather setArray start");
 	var rowData = body.RealtimeWeatherStation.row ;
+	var rowDataArray = new Array();
+	
+	for (var i = 0 ; i < rowData.length ; i++) {
+		var rowData2 = new Object();
+		
+		rowData2.SAWS_OBS_TM = rowData[i].SAWS_OBS_TM;
+		rowData2.STN_NM = rowData[i].STN_NM;
+		rowData2.STN_ID = rowData[i].STN_ID;
+		if(rowData[i].SAWS_TA_AVG =="") {
+			rowData2.SAWS_TA_AVG = parseFloat("0.0");
+		} else {
+			rowData2.SAWS_TA_AVG = parseFloat(rowData[i].SAWS_TA_AVG);
+		}
+		if(rowData[i].SAWS_HD =="") {
+			rowData2.SAWS_HD = parseFloat("0.0");
+		} else {
+			rowData2.SAWS_HD = parseFloat(rowData[i].SAWS_HD);
+		}
+		rowData2.CODE = rowData[i].CODE;
+		rowData2.NAME = rowData[i].NAME;
+		if(rowData[i].SAWS_WS_AVG =="") {
+			rowData2.SAWS_WS_AVG = parseFloat("0.0");
+		} else {
+			rowData2.SAWS_WS_AVG = parseFloat(rowData[i].SAWS_WS_AVG);
+		}
+		if(rowData[i].SAWS_RN_SUM =="") {
+			rowData2.SAWS_RN_SUM = parseFloat("0.0");
+		} else {
+			rowData2.SAWS_RN_SUM = parseFloat(rowData[i].SAWS_RN_SUM);
+		}
+		if(rowData[i].SAWS_SOLAR =="") {
+			rowData2.SAWS_SOLAR = parseFloat("0.0");
+		} else {
+			rowData2.SAWS_SOLAR = parseFloat(rowData[i].SAWS_SOLAR);
+		}
+		if(rowData[i].SAWS_SHINE =="") {
+			rowData2.SAWS_SHINE = parseFloat("0.0");
+		} else {
+			rowData2.SAWS_SHINE = parseFloat(rowData[i].SAWS_SHINE);
+		}
+		rowDataArray.push(rowData2);
+	}
+	
+	var rowData3 = JSON.stringify(rowDataArray);
+//	console.log(rowData3);
 	console.log("weather setArray end");
-	return rowData;
+	return rowData3;
 }
 
 // 매일 2시간 단위.
@@ -20,12 +65,13 @@ router.get("/", function(req, res, next) {
 	var date = newDate.addHours(-1).toFormat('YYYYMMDD');
 	var time = newDate.toFormat('HH24');
 	var url = CONFIG.weather.prevUrl + "/" + CONFIG.weather.key + "/" + CONFIG.weather.postUrl + "/1/1000";
-	
+
 	cFunc.getApiData(url, function(getErr, getRs) {
 		if( getErr ) {
 			console.log(getErr);
 			res.status(500).send();
 		} else {
+//			setArray(getRs);
 			console.log(setArray(getRs));
 			cFunc.writeXmlToJson("weather", "weather_"+date+"_"+time, getRs, function(writeErr, writeRs) {
 				if( writeErr ) {
