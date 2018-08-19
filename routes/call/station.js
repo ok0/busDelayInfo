936e0e26ae4cad7b1600da1a3d4b48e0fc36959d
han <han@DESKTOP-GFLRSJQ>
2018-08-19 14:59:10 +0900

    커밋

--- routes/call/station.js
+++ routes/call/station.js
@@ -3,10 +3,82 @@ var router = express.Router();
 var date_utils  = require('date-utils');
 var cFunc = require("../../common/func");
 const CONFIG = require("../../common/config");
+var request = require("request");
 
-// 1. trim
-// 2. parseInt(), parseFloat()
-var setArray = function() {
+var setArray = function(body) {
+	console.log("station setArray start");
+	var rowData = body.ServiceResult;
+	var rowDataArray = new Array();
+	var rowDataHeader = rowData.msgHeader[0];
+	var date = new Date();
+	var yyyy, mm, dd, hours, minutes, seconds;
+
+	for(var _key in rowData.msgBody[0].itemList){
+		var rowDataFin = new Object();
+		var tmpRowData = new Object();
+		
+		yyyy = date.getFullYear();
+		mm = (Number(date.getMonth())+1)+"";
+		if(mm.length == 1){
+			mm = "0"+mm;
+		}else{
+			mm = mm;
+		}
+		dd = date.getDate();
+		if(dd.length == 1){
+			dd = "0"+dd;
+		}else{
+			dd = dd;
+		}
+		hours = date.getHours()+"";
+		if(hours.length == 1){
+			hours = "0"+hours;
+		}else{
+			hours = hours;
+		}
+		minutes = date.getMinutes()+"";
+		if(minutes.length == 1){
+			minutes = "0"+minutes;
+		}else{
+			minutes = minutes;
+		}
+		seconds = date.getSeconds()+"";
+		if(seconds.length == 1){
+			seconds = "0"+seconds;
+		}else{
+			seconds = seconds;
+		}
+		
+		tmpRowData["regYMD"] = yyyy+""+mm+""+dd ;
+		tmpRowData["regHours"] = hours;
+		tmpRowData["regMinutes"] = minutes;
+		tmpRowData["regSeconds"] = seconds;
+
+		for(var _hKey in rowDataHeader){
+			tmpRowData[_hKey] = rowDataHeader[_hKey][0].trim();
+		}
+		
+		for(var _key2 in rowData.msgBody[0].itemList[_key]){
+			tmpRowData[_key2] = rowData.msgBody[0].itemList[_key][_key2][0].trim();
+		}
+		rowDataArray.push(tmpRowData);
+		
+		var OPT = {
+				headers: {
+					"Content-Type": "application/json"
+				}
+				, url : "http://cloud.rosesystems.kr:9200/tmp_station/doc"
+				, body : JSON.stringify(tmpRowData)
+			};
+		request.post(OPT, function(npErr, npRes, npBody) {
+			console.log(npBody);
+		});
+	}
+	
+	var resultData = JSON.stringify(rowDataArray);
+	
+	console.log("station setArray end");
+	return resultData;
 	
 }
 
@@ -38,14 +110,8 @@ router.get("/", function(req, res, next) {
 			console.log(getErr);
 			res.status(500).send();
 		} else {
-			cFunc.writeXmlToJson("station/"+today+"/", "station_"+now_time, getRs, function(writeErr, writeRs) {
-				if( writeErr ) {
-					console.log(writeErr);
-					res.status(500).send();
-				} else {
-					res.status(200).send();
-				}
-			});
+//			setArray(getRs);
+			console.log(setArray(getRs));
 		}
 	});
 });
